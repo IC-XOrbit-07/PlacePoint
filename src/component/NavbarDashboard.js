ce0ddbdaa27edd38ab09938a0a18f59a5bc9a6ee
Indian-Coder <indiancoder92@gmail.com>
2024-12-30 10:22:55 +0530

    Added some side bar and searchbar handing over to harish for further development

--- src/component/NavbarDashboard.js
+++ src/component/NavbarDashboard.js
@@ -18,7 +18,14 @@ export default function NavbarDashboard() {
                 <a className="nav-link text-light" href="#">Applied Jobs</a>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link text-light" href="#">Analytics & Reports</a>
+                <a className="nav-link text-light" href="#">Job Market Updates</a>
+                </li>
+                <li className="nav-item">
+                {/* <a className="nav-link text-light" href="#">Filter</a> */}
+
+                <button className="btn text-light nav-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" style={{border:"none",marginTop:"2.5px"}}>Filter</button>
+               
+
                 </li>
                 <li className="nav-item">
                 <a className="nav-link text-light" href="#">Settings</a>
@@ -27,8 +34,8 @@ export default function NavbarDashboard() {
             </ul>
             <form className="d-flex" role="search">
                 <input className="form-control me-2" style={{backgroundColor:"f2f2f5"}} type="search" placeholder="Search" aria-label="Search"/>
-                <button type="button" class="btn btn-primary d-flex" style={{width:"100px",alignItems:"center",gap:"10px",outline:"none !important"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <button type="button" className="btn btn-primary d-flex" style={{width:"100px",alignItems:"center",gap:"10px",outline:"none !important"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"></path>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"></path>
                     </svg>
@@ -38,6 +45,15 @@ export default function NavbarDashboard() {
             </div>
         </div>
         </nav>
+        <div style={{width:"60vw",marginTop:"50px"}} className="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel" style={{fontWeight:"bold" ,marginTop:"40px",marginLeft:"5px"}}>Filters</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style={{fontWeight:"bold" ,marginTop:"40px",marginRight:"10px"}}></button>
+          </div>
+          <div className="offcanvas-body" style={{marginLeft:"5px"}}>
+            Put filter option here but above that put a motivational card which shows motivation quotes
+          </div>
+        </div>
     </div>
   )
 }
